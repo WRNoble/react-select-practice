4e8bb61c80b0c9aace77cff152c0d04e26ba4f73
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-12-11 11:38:47 -0500

    first commit

---
+++ client/src/CustomSelect.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Select from "react-select/src/Select";
+
+const options = [{
+    label: "react", value: "react"
+}]
+
+function CustomSelect(props) {
+    return(
+        <div>
+            <Select option=
+        </div>
+    )
+}
