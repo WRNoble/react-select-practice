9914abcb5d15c330e7cdc37cfd64deb79dc7af47
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-12-11 11:55:50 -0500

    working

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,25 @@ import React from "react";
 import CustomSelect from "./CustomSelect";
 import "./App.css";
 
+const options = [
+  { label: "react", value: "react" },
+  { label: "CSS", value: "css" },
+  { label: "JavaScript", value: "js" },
+];
+
+function onChangeInput(value) {
+  console.log(value);
+}
+
 function App() {
   return (
     <div className="App">
       Select Practice
-      <CustomSelect />
+      <CustomSelect
+        label="Select A Genre"
+        onChange={onChangeInput}
+        options={options}
+      />
     </div>
   );
 }
--- client/src/CustomSelect.js
+++ client/src/CustomSelect.js
@@ -1,16 +1,11 @@
 import React from "react";
 import Select from "react-select";
 
-const options = [
-  { label: "react", value: "react" },
-  { label: "CSS", value: "css" },
-  { label: "JavaScript", value: "js" },
-];
-
-function CustomSelect(props) {
+function CustomSelect({ props, label, options, onChange }) {
   return (
     <div>
-      <Select option={options} />
+      <h2>{label}</h2>
+      <Select options={options} onChange={onChange} />
     </div>
   );
 }
